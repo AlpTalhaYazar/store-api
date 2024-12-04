512dada5df4b8a8b5ea0e4684545ca6ac46a4954
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-04 16:52:38 +0300

    feat: improve error handler middleware to utilize http-status-codes and Result utility for error responses

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,11 @@
+const { StatusCodes } = require('http-status-codes');
+const Result = require('../utils/Result');
+
 const errorHandlerMiddleware = async (err, req, res, next) => {
-  console.log(err)
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
+  const result = Result.failure(err.message);
+
+  return res.status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+    .json(result);
 }
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
