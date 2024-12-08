7672c3bc3f43f23a1c54deda2c145276045a33e6
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-08 11:11:27 +0300

    feat: update product routes to include PATCH method for updating products

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,20 @@
-import express from 'express';
-import { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/products';
+import express from "express";
+import {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/products.js";
 
 const router = express.Router();
 
-router.route('/').get(getAllProducts).post(createProduct);
-router.route('/:id').get(getProductById).put(updateProduct).delete(deleteProduct);
+router.route("/").get(getAllProducts).post(createProduct);
+
+router
+  .route("/:id")
+  .get(getProductById)
+  .patch(updateProduct)
+  .delete(deleteProduct);
 
 export { router as productsRouter };
