1b9d7a7e847e9656a30164c46eb19f1d525a31ee
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-12 13:27:54 +0300

    feat: implement CRUD operations for products with custom error handling

--- controllers/products.js
+++ controllers/products.js
@@ -1,24 +1,74 @@
+import { Product } from "../models/Product.js";
+import { Result } from "../utils/Result.js";
+import { createCustomError } from "../errors/custom-error.js";
+
 const getAllProducts = async (req, res) => {
-  res.json("All products");
+  const products = await Product.find({});
+
+  const result = Result.success(products);
+
+  res.status(200).json(result);
 };
 
 const getProductById = async (req, res) => {
   const { id } = req.params;
-  res.json(`Product ${id}`);
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    throw createCustomError(`No product with id : ${id}`, 404);
+  }
+
+  const result = Result.success(product);
+
+  res.status(200).json(result);
 };
 
 const createProduct = async (req, res) => {
-  res.json("Create product");
+  const product = new Product(req.body);
+
+  const createdProduct = await product.save();
+
+  const result = Result.success(createdProduct);
+
+  res.status(201).json(result);
 };
 
 const updateProduct = async (req, res) => {
   const { id } = req.params;
-  res.json(`Update product ${id}`);
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    throw createCustomError(`No product with id : ${id}`, 404);
+  }
+
+  var updateData = { updatedAt: Date.now(), ...req.body };
+
+  const updatedProduct = await Product.findByIdAndUpdate(id, updateData, {
+    new: true,
+    runValidators: true,
+  });
+
+  const result = Result.success(updatedProduct);
+
+  res.status(200).json(result);
 };
 
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
-  res.json(`Delete product ${id}`);
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    throw createCustomError(`No product with id : ${id}`, 404);
+  }
+
+  await Product.findByIdAndDelete(id);
+
+  const result = Result.success(null);
+
+  res.status(200).json(result);
 };
 
 export {
