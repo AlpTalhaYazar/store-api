7f272205207769347dd7544cf2280521645ba21a
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-12 10:53:46 +0300

    feat: add populate script to import products from JSON file into database

---
+++ populate.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { connectDB } from "./db/connect.js";
+import { Product } from "./models/product.js";
+import { readFile } from "fs/promises";
+
+const jsonProducts = JSON.parse(
+  await readFile(new URL("./products.json", import.meta.url))
+);
+
+const populate = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+      .then(() => console.log("Connected to the database"))
+      .catch((error) => console.error(error));
+
+    await Product.deleteMany();
+
+    await Product.insertMany(jsonProducts);
+
+    console.log("Data imported successfully");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+populate();
